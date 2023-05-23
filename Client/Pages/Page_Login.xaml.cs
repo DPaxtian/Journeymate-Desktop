f3ebb79b9b2fe99b5cddb5f1c51fb28268355260
Daniel Paxtian <daniel.paxtian@hotmail.com>
2023-05-22 21:44:04 -0600

    Creacion de UI base

---
+++ Client/Pages/Page_Login.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client.Pages
+{
+    /// <summary>
+    /// Lógica de interacción para Page_Login.xaml
+    /// </summary>
+    public partial class Page_Login : Page
+    {
+        public Page_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_ForgotPassword_Clic(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Login_Clic(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_SignUp_Clic(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
