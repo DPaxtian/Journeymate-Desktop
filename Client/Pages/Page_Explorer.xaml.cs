854f4dc19e0a53e79506f32a29ba272dfacad5d8
Axel Utrera <axeljordanomu@gmail.com>
2023-06-02 03:24:44 -0600

    Creacion de listView en explorar

--- Client/Pages/Page_Explorer.xaml.cs
+++ Client/Pages/Page_Explorer.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Models;
 
 namespace Client.Pages
 {
@@ -20,9 +21,122 @@ namespace Client.Pages
     /// </summary>
     public partial class Page_Explorer : Page
     {
+        public List<Routine> routines = new List<Routine>
+        {
+           new Routine
+           {
+               Name = "Cafeterias drag",
+               Country  = "Xalapa",
+               Routine_Description = "Antros gay(drag) para agarrar un macanon",
+               Visibility = "private",
+               label_category = "Party",
+               State_Country = "Veracruz",
+               Town = "Xalapa",
+               Creator = "Elva J. Fernández",
+               Budget = "$300"
+            },
+           new Routine
+           {
+               Name = "Cafeterias drag",
+               Country  = "Xalapa",
+               Routine_Description = "Antros gay(drag) para agarrar un macanon",
+               Visibility = "private",
+               label_category = "Party",
+               State_Country = "Veracruz",
+               Town = "Xalapa",
+               Creator = "Elva J. Fernández",
+               Budget = "$300"
+            },
+           new Routine
+           {
+               Name = "Cafeterias drag",
+               Country  = "Xalapa",
+               Routine_Description = "Antros gay(drag) para agarrar un macanon",
+               Visibility = "private",
+               label_category = "Party",
+               State_Country = "Veracruz",
+               Town = "Xalapa",
+               Creator = "Elva J. Fernández",
+               Budget = "$300"
+            },
+           new Routine
+           {
+               Name = "Cafeterias drag",
+               Country  = "Xalapa",
+               Routine_Description = "Antros gay(drag) para agarrar un macanon",
+               Visibility = "private",
+               label_category = "Party",
+               State_Country = "Veracruz",
+               Town = "Xalapa",
+               Creator = "Elva J. Fernández",
+               Budget = "$300"
+            },
+           new Routine
+           {
+               Name = "Cafeterias drag",
+               Country  = "Xalapa",
+               Routine_Description = "Antros gay(drag) para agarrar un macanon",
+               Visibility = "private",
+               label_category = "Party",
+               State_Country = "Veracruz",
+               Town = "Xalapa",
+               Creator = "Elva J. Fernández",
+               Budget = "$300"
+            },
+           new Routine
+           {
+               Name = "Cafeterias drag",
+               Country  = "Xalapa",
+               Routine_Description = "Antros gay(drag) para agarrar un macanon",
+               Visibility = "private",
+               label_category = "Party",
+               State_Country = "Veracruz",
+               Town = "Xalapa",
+               Creator = "Elva J. Fernández",
+               Budget = "$300"
+            },
+           new Routine
+           {
+               Name = "Cafeterias drag",
+               Country  = "Xalapa",
+               Routine_Description = "Antros gay(drag) para agarrar un macanon",
+               Visibility = "private",
+               label_category = "Party",
+               State_Country = "Veracruz",
+               Town = "Xalapa",
+               Creator = "Elva J. Fernández",
+               Budget = "$300"
+            }
+        };
         public Page_Explorer()
         {
             InitializeComponent();
+            List_Routines.ItemsSource = routines;
+        }
+
+       
+
+
+        private void TexBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            TextBox TextBox = (TextBox)sender;
+            TextBox.Padding = new Thickness(60, 0, 60, 0); // Ajusta el valor del Padding para el espacio deseado
+            TextBox.VerticalContentAlignment = VerticalAlignment.Center;
+        }
+
+        private void Button_Filters_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Like_Clic(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ListRoutines_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Models/Routine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//faltan atributos aun...
+namespace Models
+{
+    public class Routine
+    {
+        public string _Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Routine_Description { get; set; }
+        public string Visibility { get; set; }
+        public string label_category { get; set; }
+        public string State_Country { get; set; }
+        public string Town { get; set; }
+        public string Creator { get; set; }
+        public string Budget { get; set; }
+
+        public int CountComments { get; set; }
+
+    }
+}
+
